e252f761e200eff3ad53ff0f5d98a04ae3277eca
Brithanny Arguello <49217000+BrithannyAL@users.noreply.github.com>
2024-10-16 08:20:20 -0600

    Update test.cy.ts

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -10,27 +10,19 @@ describe('Login', function () {
     });
 
     it('username', function () {
-        cy.xpath('#las_').click().then(function () {            
+        cy.xpath('/html/body/div[2]/div/form/div[3]/div/input').click().then(function () {            
             cy.wait(200)
             cy.get('div[class="validation-summary-errors"]').should('have.text', 'Login no tuvo exito. Por favor, corrija los errores e intentelo de nuevo.\nNo cuenta encontro\n');
         });
     });
 
     it('Contraseña requerida', function () {
-        cy.get('input[name="Email"]').type('marlentrevi@gmail.com');
+        cy.get('/html/body/div[2]/div/form/div[4]/div/input').type('marlentrevi@gmail.com');
         cy.xpath('/html/body/div[4]/div[1]/div[4]/div[2]/div/div[2]/div[1]/div[2]/div[2]/form/div[5]/input').click().then(function () {            
             cy.wait(200)
             cy.get('div[class="validation-summary-errors"]').should('have.text', 'Login no tuvo exito. Por favor, corrija los errores e intentelo de nuevo.\nNo cuenta encontro\n');
         });
       });
-
-      it('Login inválido', () => {
-        cy.get('input[name="Email"]').type('marlentrevi@gmail.com');
-        cy.get('[id^=Password]').type('12345');
-        cy.xpath('/html/body/div[4]/div[1]/div[4]/div[2]/div/div[2]/div[1]/div[2]/div[2]/form/div[5]/input').click().then(function () {            
-            cy.wait(200)
-            cy.get('div[class="validation-summary-errors"]').should('have.text', 'Login no tuvo exito. Por favor, corrija los errores e intentelo de nuevo.\nNo cuenta encontro\n');
-        });
     });
 
 
